8797cfaf6b59e30cf529289e3539c77121d699a8
gm2bv <gm2bv2001@gmail.com>
2015-07-15 06:35:00 +0900

    [fix] can't parse query-parameters - it occured when no query-parameter is.

--- js/modules/UTIL.js
+++ js/modules/UTIL.js
@@ -6,11 +6,13 @@
 function getParam(key) {
     var url = location.href;
     var param = url.split("?");
-    var params = param[1].split("&");
-    for( var i=0; i < params.length; i++ ){
-        var pCols = params[i].split("=");
-        if(pCols[0] === key){
-            return pCols[1];
+    if( param[1] ){
+        var params = param[1].split("&");
+        for( var i=0; i < params.length; i++ ){
+            var pCols = params[i].split("=");
+            if(pCols[0] === key){
+                return pCols[1];
+            }
         }
     }
     return null;
